5dd0444703ae685fcd9411fbdeeadeba22bc0312
dsatria18 <demosatria36@gmail.com>
2020-03-11 13:04:39 +0700

    latihan2

--- latihan2.php
+++ latihan2.php
@@ -1,1 +1,9 @@
-sample
+<?php 
+    $A=123;//variable global
+    function Test(){
+        global $A;//variable local
+        echo "Nilai A dalam fungsi=$A\n";
+    }
+    Test();
+    echo "Nilai A luar fungsi=$A\n";
+?>
